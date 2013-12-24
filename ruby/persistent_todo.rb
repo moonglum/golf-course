f4465fc73001588a8026252cd5b3d86142d4dbe2
moonglum <moonglum@moonbeamlabs.com>
2013-12-24 09:57:02 +0100

    Fixed yesterdays version. Seems like I was too tired.

--- ruby/persistent_todo.rb
+++ ruby/persistent_todo.rb
@@ -1,6 +1,6 @@
-# ruby -rpstore -ne'(s=PStore.new ?a).transaction{a,c,j=s[0]||["\e[J"],$_.to_i,-1;puts (s[0]=c>0?a-[a[c]]:a<<$_).map{|i|"#{j+=1}: #{i}"}}'
+# ruby -rpstore -ne'(s=PStore.new ?a).transaction{a,c,j=s[0]||[],$_.to_i,0;puts "\e[2J",(s[0]=c>0?a-[a[c-1]]:a<<$_).map{|i|"#{j+=1}: #{i}"}}'
 
 require 'pstore'
 while gets
-(s=PStore.new ?a).transaction{a,c,j=s[0]||["\e[J"],$_.to_i,-1;puts (s[0]=c>0?a-[a[c]]:a<<$_).map{|i|"#{j+=1}: #{i}"}}
+(s=PStore.new ?a).transaction{a,c,j=s[0]||[],$_.to_i,0;puts "\e[2J",(s[0]=c>0?a-[a[c-1]]:a<<$_).map{|i|"#{j+=1}: #{i}"}}
 end
